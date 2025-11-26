4d35f5cc49d6cd061bca1de0b1eaf6cb978d5957
JamesNickel <mrjavadkhodashenas@gmail.com>
2025-11-26 16:03:00 +0330

    feat: tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -15,4 +15,27 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_if_response_is_cached(){
+
+        $responseA = $this->get('/api/data-a');
+        // TODO: assert response is not from cache
+        for($i = 0; $i < 5; $i++){
+            $responseA = $this->get('/api/data-a');
+        }
+        // TODO: assert response is from cache
+
+        for($i = 0; $i < 5; $i++){
+            $responseB = $this->get('/api/data-b');
+        }
+        for($i = 0; $i < 5; $i++){
+            $responseC = $this->get('/api/data-c');
+        }
+
+        $responseA = $this->get('/api/data-a');
+        // TODO: assert response is not from cache
+
+        $responseA->assertStatus(200);
+
+    }
 }
